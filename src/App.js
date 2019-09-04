9dfcc41958aba2c91311369c73bdfbe0a012608e
Patrick Coston <patcoston@gmail.com>
2019-09-04 16:51:13 -0400

    Only save deckID and added dealCard to state

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      deck: null,
-      dealer: null,
-      player: null,
-      cards: null,
+      deckID: null, // the API deckID which is used to shuffle and obtain decks of cards
+      cards: null, // will be array of 312 cards which is 6 decks of 52 cards per deck
+      dealCard: 0, // next card which is dealed
+      dealer: [], // array of indexes into cards[] array for dealers cards
+      player: [], // array of indexes into cards[] array for players cards
     };
   }
   componentDidMount() {
@@ -18,9 +19,9 @@ class App extends Component {
   getData() {
     fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6')
     .then(data => data.json())
-    .then(deck => {
-      this.setState({ deck }, () => {
-        const deckID = this.state.deck.deck_id;
+    .then(json => {
+      this.setState({ deckID: json.deck_id }, () => {
+        const deckID = this.state.deckID;
         const url = `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=312`;
         fetch(url)
         .then(data => data.json())
