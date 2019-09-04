22e68d18cad45b08f4b93d010b232becdd696fa2
Patrick Coston <patcoston@gmail.com>
2019-09-04 17:08:28 -0400

    Added method dealCards() add 2 player dealer cards

--- src/App.js
+++ src/App.js
@@ -8,13 +8,36 @@ class App extends Component {
     this.state = {
       deckID: null, // the API deckID which is used to shuffle and obtain decks of cards
       cards: null, // will be array of 312 cards which is 6 decks of 52 cards per deck
-      dealCard: 0, // next card which is dealed
+      dealCard: 0, // next card to deal
       dealer: [], // array of indexes into cards[] array for dealers cards
       player: [], // array of indexes into cards[] array for players cards
     };
   }
   componentDidMount() {
     this.getData();
+  }
+  dealCards(playerCards, dealerCards) {
+    // DEBUG: Need to add check if (dealCard + playCards + dealerCards) is greater than or equal to 312, then need to reshuffle
+    let dealCard = this.state.dealCard;
+    let player = [...this.state.player]; // clone player array
+    let dealer = [...this.state.dealer]; // clone dealer array
+    // deal player cards
+    for (let card = 0; card < playerCards; card++) {
+      player.push(dealCard++);
+    }
+    // deal dealer cards
+    for (let card = 0; card < dealerCards; card++) {
+      dealer.push(dealCard++);
+    }
+    // update which card is to be dealt next, and arrays for dealer and player cards
+    this.setState({
+      dealCard,
+      player,
+      dealer,
+    }, () => {
+      console.log('dealCards()');
+      console.log(this.state);
+    });
   }
   getData() {
     fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6')
@@ -26,7 +49,8 @@ class App extends Component {
         fetch(url)
         .then(data => data.json())
         .then(json => this.setState({ cards: json.cards }, () => {
-          console.log(this.state)
+          console.log(this.state);
+          this.dealCards(2, 2); // deal two cards to play and two cards to dealer
         }))
         .catch(err => console.log(`Deck API Fetch Error: ${err}`));
       });
