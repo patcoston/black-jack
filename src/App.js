0e65f89bd6ef8e0f3dd462218f639df52aff5d85
Patrick Coston <patcoston@gmail.com>
2019-09-07 09:04:26 -0400

    Moved get score logic back to App.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ class App extends Component {
     dealCard: 0, // next card to deal. It is an index in cards[] array
     dealer: [], // array of indexes into cards[] array for dealers cards
     player: [], // array of indexes into cards[] array for players cards
+    scores: [0, 0], // score for dealer and player
+    bust: [false, false], // did dealer or player bust?
     playerHit: this.playerHit.bind(this), // click method for player getting another card
   };
   componentDidMount() {
@@ -43,14 +45,6 @@ class App extends Component {
       console.log(this.state);
     });
   }
-  playerHit() {
-    let {player, dealCard} = this.state;
-    player.push(dealCard++); // push card on player[] array, then point to next dealCard: DEBUG: 311 is last card. Need logic to reshuffle before overflowing cards[] array. Check when Vegas reshuffles.
-    this.setState({
-      dealCard,
-      player,
-    });
-  }
   getData() {
     console.log('getData()');
     fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6')
@@ -69,8 +63,44 @@ class App extends Component {
     })
     .catch(err => console.log(`Shuffle API Fetch Error: ${err}`));
   }
+  playerHit() {
+    let {player, dealCard} = this.state;
+    player.push(dealCard++); // push card on player[] array, then point to next dealCard: DEBUG: 311 is last card. Need logic to reshuffle before overflowing cards[] array. Check when Vegas reshuffles.
+    this.setState({
+      dealCard,
+      player,
+    });
+    this.updateScore(1); // update score for player
+  }
+  updateScore(who) {
+    let score = 0;
+    let cardValue = 0;
+    const { cards, hand, scores, bust } = this.state;
+    for (let i = 0; i < hand.length; i++) {
+        const card = hand[i];
+        const value = cards[card].value;
+        if (isNaN(value)) {
+            if (value === 'ACE') {
+                cardValue = 11;
+            } else {
+                cardValue = 10;
+            }
+        } else {
+            cardValue = parseInt(value);
+        }
+        score += cardValue;
+    }
+    if (score > 21) { // if score > 21
+        bust[who] = true; // dealer or player bust
+    }
+    scores[who] = score; // dealer or player score
+    this.setState({
+      scores,
+      bust,
+    })
+}
   render() {
-    const { cards, dealer, player, playerHit } = this.state;
+    const { cards, dealer, player, playerHit,scores } = this.state;
     if (!cards || !dealer.length || !player.length) {
       return (
         <div>Loading ...</div>
@@ -79,11 +109,11 @@ class App extends Component {
     return (
       <div className="App">
         <section>
-          <h2>Dealer <Score cards={cards} hand={dealer} who={0} /></h2>
+          <h2>Dealer <Score cards={cards} hand={dealer} score={scores[0]} /></h2>
           <Hand cards={cards} hand={dealer} />
         </section>
         <section>
-          <h2>Player <Score cards={cards} hand={player} who={1} /></h2>
+          <h2>Player <Score cards={cards} hand={player} score={scores[1]} /></h2>
           <Hand cards={cards} hand={player} />
           <Actions hit={playerHit} />
         </section>
--- src/Score.js
+++ src/Score.js
@@ -6,40 +6,13 @@ class Score extends Component {
         this.state = {
             cards: props.cards, // array of all cards
             hand: props.hand, // array of indexes into cards array for that hand
-            scores: [0, 0], // score for dealer and player
-            bust: [false, false], // did dealer or player bust?
-            who: props.who, // 0=dealer 1=player, used as indexes into score[] array when call udpateScore()
+            score: props.score, // score of dealer or player
         }
-    }
-    componentWillUnmount() {
-        console.log('componentWillUnmount()');
-        let score = 0;
-        let cardValue = 0;
-        const { cards, hand, scores, bust, who } = this.state;
-        for (let i = 0; i < hand.length; i++) {
-            const card = hand[i];
-            const value = cards[card].value;
-            if (isNaN(value)) {
-                if (value === 'ACE') {
-                    cardValue = 11;
-                } else {
-                    cardValue = 10;
-                }
-            } else {
-                cardValue = parseInt(value);
-            }
-            score += cardValue;
-        }
-        if (score > 21) { // if score > 21
-            bust[who] = true; // dealer or player bust
-        }
-        scores[who] = score; // dealer or player score
-        this.setState({scores})
     }
     render() {
-        const { scores, who } = this.state;
+        const { score } = this.state;
         return (
-            <span>Score: {scores[who]}</span>
+            <span>Score: {score}</span>
         );
     }
 }
